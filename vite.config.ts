d844fcd00644674ff97e4bced952979937fa5ee0
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2023-03-22 18:09:33 +0200

    necessary

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
+import { angular } from '@builder.io/qwik-angular/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),  angular({
+      tsconfig: './tsconfig.json', 
+      componentsDir: 'integrations/angular'
+  })],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
